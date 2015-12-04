5da39c92de124f9a2dce5a88c8ed7788778348f3
tigredonorte <tigredonorte3@gmail.com>
2015-12-04 13:23:16 -0200

    xmlImprovements

--- src/xmlResource.php
+++ src/xmlResource.php
@@ -3,41 +3,58 @@
 class xmlResource extends \classes\Interfaces\resource{
     
     /**
-    * retorna a instância do banco de dados
-    * @uses Faz a chamada do contrutor
-    * @throws DBException
-    * @return retorna um objeto com a instância do banco de dados
+    * returns a xmlResource instance
+    * @uses call xmlResource's constructor
+    * @return returns a xmlResource instance
     */
     private static $instance = NULL;
     public static function getInstanceOf(){
-        
         $class_name = __CLASS__;
-        if (!isset(self::$instance)) {
-            self::$instance = new $class_name;
-        }
-
+        if (!isset(self::$instance)) {self::$instance = new $class_name;}
         return self::$instance;
     }
     
      /** 
      * xml2array() will convert the given XML text to an array in the XML structure. 
      * Link: http://www.bin-co.com/php/scripts/xml2array/ 
-     * Arguments : $contents - The XML text 
- *                $get_attributes - 1 or 0. If this is 1 the function will get the attributes as well as the tag values - this results in a different array structure in the return value.
- *                $priority - Can be 'tag' or 'attribute'. This will change the way the resulting array sturcture. For 'tag', the tags are given more importance.
-     * Return: The parsed XML in an array form. Use print_r() to see the resulting array structure. 
-     * Examples: $array =  xml2array(file_get_contents('feed.xml')); 
-     *              $array =  xml2array(file_get_contents('feed.xml', 1, 'attribute')); 
+     * @param string $contents - The XML text 
+     * 
+     * @return The parsed XML in an array form. Use print_r() to see the resulting array structure.
      */ 
     public function xml2array($contents) { 
         require_once dirname(__FILE__).'/classes/XML2Array.php';
         return XML2Array::createArray($contents);
     }
     
-    public function array2xml($array) { 
-        require_once dirname(__FILE__).'/classes/XML2Array.php';
-        return XML2Array::convert($array);
+    /** 
+     * array2xml will convert the given array to XML text.
+     * @param array $array multi dimensional array to be converted in XML data
+     * @param string $file [optional] If specified, the function writes the data to the file rather than returning it.
+     * 
+     * @return mixed If the filename isn't specified, this function returns a XML string on success and FALSE on error. 
+     * If the parameter is specified, it returns TRUE if the file was written successfully and FALSE otherwise.
+     */ 
+    public function array2xml($array, $file = "") { 
+        $xml = new SimpleXMLElement('<?xml version="1.0"?><data></data>');
+        $this->convertArray2xml($array, $xml);
+        if($file != ""){return $xml->asXML($file);}
+        return $xml->asXML();
     }
+    
+            private function convertArray2xml( $data, &$xml) {
+                foreach( $data as $key => $value ) {
+                    if( is_array($value) ) {
+                        if( is_numeric($key) ){
+                            $key = 'item'.$key; //dealing with <0/>..<n/> issues
+                        }
+                        $subnode = $xml->addChild($key);
+                        $this->convertArray2xml($value, $subnode);
+                    } else {
+                        $xml->addChild("$key",htmlspecialchars("$value"));
+                    }
+                 }
+            }
+
     
     public function getXmlArray($file){
         if(!file_exists($file)){
@@ -50,5 +67,3 @@ class xmlResource extends \classes\Interfaces\resource{
     }
 
 }
-
-?>
